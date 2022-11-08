7bd99c5880642f373a42f6a3d4e2e293982b2a5a
khachoangpt <hoangncd3@gmail.com>
2022-11-09 00:13:02 +0700

    hide remove btn

--- pages/blogs/[id]/index.tsx
+++ pages/blogs/[id]/index.tsx
@@ -56,11 +56,11 @@ const BlogDetail = () => {
           <div>
             <Typography.Text>{blog.content}</Typography.Text>
           </div>
-          <div style={{ padding: "24px", float: "right" }}>
+          {/* <div style={{ padding: "24px", float: "right" }}>
             <Button type="primary" danger size="large" onClick={handleRemove}>
               Delete blog
             </Button>
-          </div>
+          </div> */}
         </div>
       }
     />
